cf86bce774af6cf3830205ca5dc99c78e33595b9
Ahmadzakix <78718098+Ahmadzakix@users.noreply.github.com>
2021-02-28 21:11:11 +0700

    Add files via upload

---
+++ database/menu/help.js
@@ -0,0 +1,57 @@
+const help = (pushname, prefix, botName, ownerName, reqXp, uangku, _registered, role, time, date) => {
+        return `
+
+╔══✪〘 INFO  USER 〙✪════
+╠➥ *Name : ${pushname}
+╠➥ *Role : ${role}
+╠➥ *Uang* : ${uangku}
+╠➥ *Xp* : ${reqXp}
+╠➥ *Owner : Ahmadzaki*
+╠➥ *Total Pengguna Terdaftar : ${_registered.length}
+╠➥ *Time : ${time}
+╠➥ *Calendar: ${date}
+╠➥ *Days : ${timi}
+╠═════════════════════
+║ *〘 PERATURAN BOT 〙*             
+╠═════════════════════
+╠➥ *DILARANG VC/CALL BOT*
+╠➥ *ALAU TELEPON/VC AUTO BLOCK*
+╠➥ *DILARANG MEMBULLY BOT*
+╠➥ *KALAU ADA BUG LAPOR OWNER*
+╠➥ *MAAF JIKA ADA FITUR ERORR*
+╠═════════════════════
+╠➥ *${prefix}info*
+╠➥ *${prefix}donasi*
+╠➥ *${prefix}owner*
+╠═════════════════════
+║  *〘 LIST MENU 〙*               
+╠═════════════════════
+╠➥ *${prefix}logomakermenu〘 Allmenu Erorr〙*
+╠➥ *${prefix}imagemakermenu*
+╠➥ *${prefix}stickermakermenu*
+╠➥ *${prefix}searchmenu*
+╠➥ *${prefix}educationmenu*
+╠➥ *${prefix}kerangmenu*
+╠➥ *${prefix}downloadermenu*
+╠➥ *${prefix}mememenu*
+╠➥ *${prefix}groupmenu*
+╠➥ *${prefix}soundmenu*
+╠➥ *${prefix}musicmenu*
+╠➥ *${prefix}islammenu*
+╠➥ *${prefix}stalkmenu*
+╠➥ *${prefix}wibumenu*
+╠➥ *${prefix}18+menu*
+╠➥ *${prefix}funmenu*
+╠➥ *${prefix}todmenu*
+╠➥ *${prefix}informationmenu*
+╠➥ *${prefix}stayonscreenmenu*
+╠➥ *${prefix}xpmenu*
+╠➥ *${prefix}limitmenu*
+╠➥ *${prefix}ownermenu*
+╠➥ *${prefix}othermenu*
+╠➥ *${prefix}getmenu*
+╠═════════════════════
+║  *Developer ©HLX-BOT*          
+╚═════════════════════`
+}
+exports.help = help
